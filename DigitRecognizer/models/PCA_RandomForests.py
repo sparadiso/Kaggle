42478ccfdc2feac771b83a4844f78468de92d504
Sean Paradiso <seanparadiso@gmail.com>
2015-02-15 00:53:09 -0800

    Updated random forests model to test a grid search over n_estimators to check for saturation

--- DigitRecognizer/models/PCA_RandomForests.py
+++ DigitRecognizer/models/PCA_RandomForests.py
@@ -11,22 +11,14 @@ X, y = TrainData()
 model_name = "model_RandomForests.pkl"
 
 # Build the pipeline
-rfs = RandomForestClassifier(n_jobs=1)
-pca = decomposition.PCA(n_components=150)
+rfs = RandomForestClassifier(n_jobs=2)
+pca = decomposition.PCA(n_components=400)
 pipe = Pipeline(steps=[('pca', pca), ('normalize', StandardScaler()), ('rfs', rfs)])
+
+# Do a quick grid search for n_estimator convergence
+for nest in [15, 50, 100, 200, 350]:
+    rfs.n_estimators = nest
+    print "NEstimators = {}, score = {}".format(nest, ScoreModel(pipe, X, y))
 
 with open(model_name, 'wb') as f:
     pickle.dump(pipe, f)
-
-# Set a few model parameters
-params = dict(rfs__n_estimators=[10, 50, 150, 250])
-
-grid = GridSearchCV(pipe, params, n_jobs=3, verbose=5, cv=3)
-grid.fit(X, y)
-
-best = grid.best_estimator_
-print best.named_steps['rfs'].n_estimators
-
-with open(model_name, 'wb') as f:
-    pickle.dump(grid, f)
-
